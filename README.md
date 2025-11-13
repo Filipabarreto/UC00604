2b34006b2ff716316fa376be80d5519c6573166c
FilipaBarreto <replayandreplay@gmail.com>
2025-11-13 21:36:34 +0000

    inicio projecto

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Uc00604
+# 00604
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 20.3.9.
 
--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('uc00604');
+  protected readonly title = signal('00604');
 }
