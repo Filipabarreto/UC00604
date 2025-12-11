685231635826dee4aa8fd90796887bf0c7da5921
FilipaBarreto <replayandreplay@gmail.com>
2025-12-11 22:47:12 +0000

    alteraçao de authentication para letra maiuscula

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,10 @@ export const routes: Routes = [
   /**nossa primeira rota */
   {path:'', redirectTo:'welcome', pathMatch:'full'},
   {path:'welcome', loadComponent:() => import('./pages/welcome/welcome').then(c => c.Welcome)},
-  {path:"pages/authentication", loadComponent:() => import('./pages/authentication/authentication').then(c => c.Authentication)},
+  {path:"pages/authentication", loadComponent:() => import('./pages/Authentication/authentication').then(c => c.Authentication)},
   /**rota padrao sem lazyload */
   {path:"pages/uc00604", component:Uc00604},
-  
+
 /**nossa ultima rota */
   {path:'**', loadComponent:() => import('./pages/welcome/welcome').then(filipa => filipa.Welcome)},
 ];
