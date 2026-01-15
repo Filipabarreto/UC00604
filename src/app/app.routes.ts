9fe8c517f3e1ba81eebf0330967c0380660f582e
FilipaBarreto <replayandreplay@gmail.com>
2026-01-15 21:54:50 +0000

    correcção do erro na authentication que tinha / a mais

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { Uc00604 } from './pages/uc00604/uc00604';
 export const routes: Routes = [
   /**nossa primeira rota */
   {path:'', redirectTo:'welcome', pathMatch:'full'},
-  {path:'welcome', loadComponent:() => import('./pages/welcome/welcome').then(c => c.Welcome)},
-  {path:"pages/Authentication", loadComponent:() => import('./pages/Authentication/authentication').then(c => c.Authentication)},
-  /**rota padrao sem lazyload */
+
   {path:"pages/uc00604", component:Uc00604},
+  {path:'welcome', loadComponent:() => import('./pages/welcome/welcome').then(c => c.Welcome)},
+  {path:"pages/authentication", loadComponent:() => import('./pages/Authentication/authentication').then(c => c.Authentication)},
+  /**rota padrao sem lazyload */
+
 
 /**nossa ultima rota */
   {path:'**', loadComponent:() => import('./pages/welcome/welcome').then(filipa => filipa.Welcome)},
